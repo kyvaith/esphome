0dcb689ef9d2dd1c26fd44f457d8a3e32bcd285c
Tomasz Witke <kyvaith@gmail.com>
2026-06-24 20:49:33 +0200

    Revert "[artwork_image] Defer SendSpin artwork presentation"
    This reverts commit fc9d91fd37e395c125cfca270ef493436ff1a7d4.

--- esphome/components/artwork_image/artwork_image.cpp
+++ esphome/components/artwork_image/artwork_image.cpp
@@ -255,11 +255,14 @@ void ArtworkImage::setup() {
         return;
       }
     }
-    if (!this->sendspin_decode_failed_.load(std::memory_order_acquire) &&
-        !this->sendspin_decode_ready_.load(std::memory_order_acquire)) {
+    if (this->sendspin_decode_failed_.exchange(false, std::memory_order_acq_rel)) {
+      this->download_error_callback_.call();
       return;
     }
-    this->queue_sendspin_finish_();
+    if (!this->sendspin_decode_ready_.exchange(false, std::memory_order_acq_rel)) {
+      return;
+    }
+    this->finish_download_();
   });
 #endif
 }
@@ -330,26 +333,13 @@ void ArtworkImage::process_pending_sendspin_() {
   if (!display) {
     return;
   }
-  if (this->sendspin_decode_failed_.load(std::memory_order_acquire) ||
-      this->sendspin_decode_ready_.load(std::memory_order_acquire)) {
-    this->queue_sendspin_finish_();
-  }
-}
-
-void ArtworkImage::queue_sendspin_finish_() {
-  if (this->sendspin_finish_queued_.exchange(true, std::memory_order_acq_rel)) {
+  if (this->sendspin_decode_failed_.exchange(false, std::memory_order_acq_rel)) {
+    this->download_error_callback_.call();
     return;
   }
-  this->defer([this]() {
-    this->sendspin_finish_queued_.store(false, std::memory_order_release);
-    if (this->sendspin_decode_failed_.exchange(false, std::memory_order_acq_rel)) {
-      this->download_error_callback_.call();
-      return;
-    }
-    if (this->sendspin_decode_ready_.exchange(false, std::memory_order_acq_rel)) {
-      this->finish_download_();
-    }
-  });
+  if (this->sendspin_decode_ready_.exchange(false, std::memory_order_acq_rel)) {
+    this->finish_download_();
+  }
 }
 #endif
 
