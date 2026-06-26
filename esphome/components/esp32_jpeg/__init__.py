b46049f3162c476944e52d81ee1eef4929f0fa52
Tomasz Witke <kyvaith@gmail.com>
2026-06-26 18:08:49 +0200

    [esp32_jpeg] Expose DMA2D QoS tuning

--- esphome/components/esp32_jpeg/__init__.py
+++ esphome/components/esp32_jpeg/__init__.py
@@ -8,6 +8,16 @@ DEPENDENCIES = ["esp32"]
 CODEOWNERS = ["@kyvaith"]
 
 CONF_DECODER_TIMEOUT = "decoder_timeout"
+CONF_DMA2D_AXI_BURSTINESS = "dma2d_axi_burstiness"
+CONF_DMA2D_PEAK_LEVEL = "dma2d_peak_level"
+CONF_DMA2D_TRANSACTION_LEVEL = "dma2d_transaction_level"
+CONF_DMA2D_WRITE_PRIORITY = "dma2d_write_priority"
+CONF_DMA2D_READ_PRIORITY = "dma2d_read_priority"
+CONF_JPEG_DMA2D_AXI_BURSTINESS = "jpeg_dma2d_axi_burstiness"
+CONF_JPEG_DMA2D_PEAK_LEVEL = "jpeg_dma2d_peak_level"
+CONF_JPEG_DMA2D_TRANSACTION_LEVEL = "jpeg_dma2d_transaction_level"
+CONF_JPEG_DMA2D_WRITE_PRIORITY = "jpeg_dma2d_write_priority"
+CONF_JPEG_DMA2D_READ_PRIORITY = "jpeg_dma2d_read_priority"
 
 esp32_jpeg_ns = cg.esphome_ns.namespace("esp32_jpeg")
 Esp32JpegComponent = esp32_jpeg_ns.class_("Esp32JpegComponent", cg.Component)
@@ -17,6 +27,16 @@ CONFIG_SCHEMA = cv.All(
         {
             cv.GenerateID(): cv.declare_id(Esp32JpegComponent),
             cv.Optional(CONF_DECODER_TIMEOUT, default="180ms"): cv.positive_time_period_milliseconds,
+            cv.Optional(CONF_DMA2D_AXI_BURSTINESS, default=8): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_DMA2D_PEAK_LEVEL, default=0): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_DMA2D_TRANSACTION_LEVEL, default=1): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_DMA2D_WRITE_PRIORITY, default=1): cv.int_range(min=0, max=3),
+            cv.Optional(CONF_DMA2D_READ_PRIORITY, default=1): cv.int_range(min=0, max=3),
+            cv.Optional(CONF_JPEG_DMA2D_AXI_BURSTINESS, default=1): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_JPEG_DMA2D_PEAK_LEVEL, default=2): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_JPEG_DMA2D_TRANSACTION_LEVEL, default=4): cv.int_range(min=0, max=15),
+            cv.Optional(CONF_JPEG_DMA2D_WRITE_PRIORITY, default=0): cv.int_range(min=0, max=3),
+            cv.Optional(CONF_JPEG_DMA2D_READ_PRIORITY, default=0): cv.int_range(min=0, max=3),
         }
     ).extend(cv.COMPONENT_SCHEMA),
     cv.only_on_esp32,
@@ -30,4 +50,17 @@ async def to_code(config):
     await cg.register_component(var, config)
     cg.add(var.set_decoder_timeout(config[CONF_DECODER_TIMEOUT].total_milliseconds))
     cg.add_define("USE_ESP32_JPEG")
+    cg.add_define("CONFIG_ESPHOME_DMA2D_AXI_BURSTINESS", config[CONF_DMA2D_AXI_BURSTINESS])
+    cg.add_define("CONFIG_ESPHOME_DMA2D_PEAK_LEVEL", config[CONF_DMA2D_PEAK_LEVEL])
+    cg.add_define("CONFIG_ESPHOME_DMA2D_TRANSACTION_LEVEL", config[CONF_DMA2D_TRANSACTION_LEVEL])
+    cg.add_define("CONFIG_ESPHOME_DMA2D_WRITE_PRIORITY", config[CONF_DMA2D_WRITE_PRIORITY])
+    cg.add_define("CONFIG_ESPHOME_DMA2D_READ_PRIORITY", config[CONF_DMA2D_READ_PRIORITY])
+    cg.add_define("CONFIG_ESPHOME_JPEG_DMA2D_AXI_BURSTINESS", config[CONF_JPEG_DMA2D_AXI_BURSTINESS])
+    cg.add_define("CONFIG_ESPHOME_JPEG_DMA2D_PEAK_LEVEL", config[CONF_JPEG_DMA2D_PEAK_LEVEL])
+    cg.add_define(
+        "CONFIG_ESPHOME_JPEG_DMA2D_TRANSACTION_LEVEL",
+        config[CONF_JPEG_DMA2D_TRANSACTION_LEVEL],
+    )
+    cg.add_define("CONFIG_ESPHOME_JPEG_DMA2D_WRITE_PRIORITY", config[CONF_JPEG_DMA2D_WRITE_PRIORITY])
+    cg.add_define("CONFIG_ESPHOME_JPEG_DMA2D_READ_PRIORITY", config[CONF_JPEG_DMA2D_READ_PRIORITY])
     esp32.include_builtin_idf_component("esp_driver_jpeg")
